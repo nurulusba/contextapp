0fe2b2662d0e1adf2c5c7c3e49df11a438e0f86a
noor ul usba <noorulusba@gmail.com>
2020-06-13 18:30:08 +0500

    First Commit

---
+++ src/components/BookList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class BookList extends Component {
+    render() {
+        return (
+            <div className='book-list-container'>
+                <ul>
+                    <li>the way of kings</li>
+                    <li>the name of winds</li>
+                    <li>the final empire</li>
+
+                </ul>
+            </div>
+        )
+    }
+}
+export default BookList;
